0feabdb5f50910735fd6738c741c973f24d460ce
marc-19 <001marc.clotet@gmail.com>
2024-11-26 17:11:05 +0100

    params results working

--- app/controllers/predictions_controller.rb
+++ app/controllers/predictions_controller.rb
@@ -1,18 +1,21 @@
 class PredictionsController < ApplicationController
   def new
     @quiniela = Quiniela.find(params[:quiniela_id])
-    @local_teams = @quiniela.local_teams
-    @visitor_teams = @quiniela.visitor_teams
+    set_teams
     @prediction = Prediction.new
   end
 
   def create
     @quiniela = Quiniela.find(params[:quiniela_id])
+    set_teams
     @prediction = current_user.predictions.new(prediction_params)
+
+    # Extract and assign the result array
+    @prediction.result = params[:prediction][:result].values
     @prediction.quiniela = @quiniela
 
     if @prediction.save
-      redirect_to quinielas_path, notice: "Your prediction has been submitted!"
+      redirect_to home_path, notice: "Your prediction has been submitted!"
     else
       flash.now[:alert] = "There was an error submitting your prediction."
       render :new, status: :unprocessable_entity
@@ -22,7 +25,12 @@ class PredictionsController < ApplicationController
   private
 
   def prediction_params
-    params.require(:prediction).permit(result: [])
+    params.require(:prediction).permit(result: []) # Permit an array of results
+  end
+
+  def set_teams
+    # Clean the local and visitor teams arrays
+    @local_teams = @quiniela.local_teams.map(&:strip)
+    @visitor_teams = @quiniela.visitor_teams.map(&:strip)
   end
 end
-
--- app/models/prediction.rb
+++ app/models/prediction.rb
@@ -1,4 +1,6 @@
 class Prediction < ApplicationRecord
   belongs_to :user
   belongs_to :quiniela
+
+  validates :result, presence: true
 end
