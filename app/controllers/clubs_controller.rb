86bebd49ceeed77ba1ceebb0a5be69734fa7a690
marc-19 <001marc.clotet@gmail.com>
2024-11-27 11:53:55 +0100

    user just can submit one prediction for quiniela

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -1,13 +1,14 @@
 class ClubsController < ApplicationController
-  
-  def show
-    @club = Club.find(params[:id])
-    @active_quinielas = @club.quinielas.where("start_date > ?", DateTime.now)
-    @past_quinielas = @club.quinielas.where("end_date < ?", DateTime.now)
-    
+
   def index
     @clubs = Club.all
     query = params[:query]
     @clubs = Club.where("name ILIKE ?", "%#{query}%")
+  end
+
+  def show
+    @club = Club.find(params[:id])
+    @active_quinielas = @club.quinielas.where("start_date > ?", DateTime.now)
+    @past_quinielas = @club.quinielas.where("end_date < ?", DateTime.now)
   end
 end
