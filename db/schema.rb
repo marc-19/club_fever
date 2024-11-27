be2a73afe550a1c8b6e960faa8d00bbea65041b3
Enrico Mann <enrico_mann@hotmail.com>
2024-11-27 15:07:27 +0100

    blabla3

--- db/schema.rb
+++ db/schema.rb
@@ -27,12 +27,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_27_085040) do
   create_table "active_storage_blobs", force: :cascade do |t|
     t.string "key", null: false
     t.string "filename", null: false
-    t.stringse
-    t.string "checksum"
-    t.datetime "created_at", null:  "content_type"
+    t.string "content_type"
     t.text "metadata"
     t.string "service_name", null: false
-    t.bigint "byte_size", null: falfalse
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
