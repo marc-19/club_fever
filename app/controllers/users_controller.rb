647c2e46eed2d980b21b2ecf426283309e010610
marc-19 <001marc.clotet@gmail.com>
2024-12-04 11:16:05 +0100

    all working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,10 @@ class UsersController < ApplicationController
     if @user != current_user
       redirect_to root_path, alert: "You are not authorized to access this page."
     end
+    
     @past_predictions = @user.predictions.includes(:quiniela)
+    @playable_quinielas = current_user.followed_clubs.includes(:quinielas).flat_map do |club|
+      club.quinielas.playable.reject { |quiniela| @past_predictions.map(&:quiniela_id).include?(quiniela.id) }
+    end
   end
 end
