0f5a81ba66109872be127d8193250759fc5cbdc5
emmamarlie <eschstruth.em@gmail.com>
2024-12-05 09:40:32 +0100

    W

--- app/javascript/controllers/confirmation_controller.js
+++ app/javascript/controllers/confirmation_controller.js
@@ -5,18 +5,56 @@ export default class extends Controller {
 
   connect() {
     console.log("Confirmation controller connected");
+    this.checkIfConfirmed();
+  }
+
+  checkIfConfirmed() {
+    if (localStorage.getItem('confirmationShown')) {
+      // Hide the confirmation popup if it has been shown before
+      this.rootConfirmationTarget.style.display = 'none';
+    }
   }
 
   openConfirmation(event) {
     event.preventDefault();
     console.log("Opening confirmation popup");
-    this.rootConfirmationTarget.style.display = "block";
-    this.form = event.target.closest('form');
+
+    // Show confirmation popup only if it hasn't been shown before
+    if (!localStorage.getItem('confirmationShown')) {
+      this.rootConfirmationTarget.style.display = "block";
+      this.form = event.target.closest('form');
+    }
   }
 
   closeConfirmation() {
     console.log("Closing confirmation popup");
     this.rootConfirmationTarget.style.display = "none";
     this.form = null;
+
+    // Set this flag to indicate the confirmation has been shown
+    localStorage.setItem('confirmationShown', 'true');
   }
 }
+
+// import { Controller } from "@hotwired/stimulus";
+
+// export default class extends Controller {
+//   static targets = ["rootConfirmation", "confirmationContent"];
+
+//   connect() {
+//     console.log("Confirmation controller connected");
+//   }
+
+//   openConfirmation(event) {
+//     event.preventDefault();
+//     console.log("Opening confirmation popup");
+//     this.rootConfirmationTarget.style.display = "block";
+//     this.form = event.target.closest('form');
+//   }
+
+//   closeConfirmation() {
+//     console.log("Closing confirmation popup");
+//     this.rootConfirmationTarget.style.display = "none";
+//     this.form = null;
+//   }
+// }
